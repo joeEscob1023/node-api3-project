a120bf1cd37b5bed58e901b6014644aa61839676
Joe Escobedo <joe_escob1023@live.com>
2021-09-29 12:58:37 -0700

    Trying to set up server, getting alot of errors

--- api/middleware/middleware.js
+++ api/middleware/middleware.js
@@ -1,5 +1,7 @@
 function logger(req, res, next) {
   // DO YOUR MAGIC
+  console.log(`it is a ${req.method} request to ${req.originalUrl}`);
+  next();
 }
 
 function validateUserId(req, res, next) {
@@ -15,3 +17,6 @@ function validatePost(req, res, next) {
 }
 
 // do not forget to expose these functions to other modules
+module.exports = {
+  logger,
+};
--- api/server.js
+++ api/server.js
@@ -1,12 +1,18 @@
-const express = require('express');
+const express = require("express");
 
 const server = express();
+
+const userRouter = require("./users/users-router");
+
+server.use(express.json());
 
 // remember express by default cannot parse JSON in request bodies
 
 // global middlewares and the user's router need to be connected here
 
-server.get('/', (req, res) => {
+server.use("/api/users", userRouter);
+
+server.get("/", (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
 });
 
