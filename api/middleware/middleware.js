e76dcd996c47d4eb4c60937fc0261bd63dcd457f
Joe Escobedo <joe_escob1023@live.com>
2021-09-29 15:31:20 -0700

    had conditions wrong in middleware, but theyre fiixed and all tests pass

--- api/middleware/middleware.js
+++ api/middleware/middleware.js
@@ -29,7 +29,7 @@ async function validateUserId(req, res, next) {
 function validateUser(req, res, next) {
   // DO YOUR MAGIC
   const { name } = req.body;
-  if (!name || name.trim()) {
+  if (!name || !name.trim()) {
     res.status(400).json({
       message: "missing required name field",
     });
@@ -42,7 +42,7 @@ function validateUser(req, res, next) {
 function validatePost(req, res, next) {
   // DO YOUR MAGIC
   const { text } = req.body;
-  if (!text || text.trim()) {
+  if (!text || !text.trim()) {
     res.status(400).json({
       message: "missing required text field",
     });
