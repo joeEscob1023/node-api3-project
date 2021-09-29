d014c9294a7c24016241a64e55703e665b2479f1
Joe Escobedo <joe_escob1023@live.com>
2021-09-29 13:18:44 -0700

    logger middleware works

--- api/server.js
+++ api/server.js
@@ -1,10 +1,11 @@
 const express = require("express");
-
+const { logger } = require("./middleware/middleware");
 const server = express();
 
 const userRouter = require("./users/users-router");
 
 server.use(express.json());
+server.use(logger);
 
 // global middlewares and the user's router need to be connected here
 
