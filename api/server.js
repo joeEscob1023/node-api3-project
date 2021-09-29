c0cc80e9cdd7d3a8ad8575bf2ba9836ce3af3b46
Joe Escobedo <joe_escob1023@live.com>
2021-09-29 13:09:44 -0700

    got server to run again

--- api/server.js
+++ api/server.js
@@ -6,11 +6,9 @@ const userRouter = require("./users/users-router");
 
 server.use(express.json());
 
-// remember express by default cannot parse JSON in request bodies
-
 // global middlewares and the user's router need to be connected here
 
-server.use("/api/users", userRouter);
+//server.use("/api/users", userRouter);
 
 server.get("/", (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
